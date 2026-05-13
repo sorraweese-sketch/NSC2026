fe6468febb7d82e501ea0d5f8e9db679c1e86a4f
sorraweese-sketch <sorrawee.se@gmail.com>
2026-05-13 17:43:32 +0700

    add_water

---
+++ Assets/Scenes/Water/Watermove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WaterMove : MonoBehaviour
+{
+    void Update()
+    {
+        // ทำให้หน้าไหลไปเรื่อยๆ ตามเวลา
+        float speed = 0.1f;
+        float offset = Time.time * speed;
+
+        // ใช้คำสั่งนี้เพื่อเลื่อน Offset ของ Texture
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offset, 0);
+    }
+}
