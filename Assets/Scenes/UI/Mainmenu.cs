fd27062db50c8205de9f21e956432ecf68f95bd5
sorraweese-sketch <sorrawee.se@gmail.com>
2026-05-17 15:26:19 +0700

    add Uimainmenu

---
+++ Assets/Scenes/UI/Mainmenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Open Settings");
+    }
+}
